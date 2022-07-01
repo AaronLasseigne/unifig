6f1dd0359c8a68dc1a415e97580ebc546bd917fc
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2022-07-01 13:26:27 -0500

    fix doc

--- lib/unifig/init.rb
+++ lib/unifig/init.rb
@@ -14,6 +14,7 @@ module Unifig
     #   YML
     #
     # @param str [String] A YAML config.
+    # @param env [Symbol] An environment to load.
     #
     # @raise [YAMLSyntaxError]
     def self.load(str, env)
